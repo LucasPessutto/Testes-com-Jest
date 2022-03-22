72896f0958bcbd5534474436593ad6a0b858851e
LucasPessutto <lucas.pessutto1@gmail.com>
2022-03-22 13:49:16 -0300

    Aplicando o metodo de desconto e instalando a lib Dinero

--- src/lib/carrinho.js
+++ src/lib/carrinho.js
@@ -1,5 +1,46 @@
 import find from "lodash/find";
 import remove from "lodash/remove";
+import Dinero from "dinero.js";
+
+const calcularPorcentagemDesconto = (amount, { condicao, quantidade }) => {
+  if (condicao?.porcentagem && quantidade > condicao.minimo) {
+    return amount.percentage(condicao.porcentagem);
+  }
+  return dinheiro({ amount: 0 });
+};
+
+const calcularQuatidadeDesconto = (amount, { condicao, quantidade }) => {
+  const par = quantidade % 2 === 0;
+  if (condicao?.quantidade && quantidade > condicao.quantidade) {
+    return amount.percentage(par ? 50 : 40);
+  }
+  return dinheiro({ amount: 0 });
+};
+
+const calcularDesconto = (amount, quantidade, condicao) => {
+  const lista = Array.isArray(condicao) ? condicao : [condicao];
+
+  const [maiorDesconto] = lista
+    .map((cond) => {
+      if (cond.porcentagem) {
+        return calcularPorcentagemDesconto(amount, {
+          condicao: cond,
+          quantidade,
+        }).getAmount();
+      } else if (cond.quantidade) {
+        return calcularQuatidadeDesconto(amount, {
+          condicao: cond,
+          quantidade,
+        }).getAmount();
+      }
+    })
+    .sort((a, b) => b - a);
+  return dinheiro({ amount: maiorDesconto });
+};
+
+const dinheiro = Dinero;
+
+(dinheiro.defaultCurrency = "BRL"), (dinheiro.defaultPrecision = 2);
 
 export default class Carrinho {
   items = [];
@@ -18,17 +59,28 @@ export default class Carrinho {
   }
 
   pegarTotal() {
-    return this.items.reduce((acc, item) => {
-      return acc + item.quantidade * item.produto.preco;
-    }, 0);
+    return this.items.reduce((acc, { quantidade, produto, condicao }) => {
+      const amount = dinheiro({ amount: quantidade * produto.preco });
+      let desconto = dinheiro({ amount: 0 });
+
+      if (condicao) {
+        desconto = calcularDesconto(amount, quantidade, condicao);
+      }
+
+      return acc.add(amount).subtract(desconto);
+    }, dinheiro({ amount: 0 }));
   }
 
   summary() {
     const total = this.pegarTotal();
+    const formatado = total.toFormat("$0,0.00");
     const items = this.items;
+
+    console.log(formatado);
 
     return {
       total,
+      formatado,
       items,
     };
   }
@@ -39,7 +91,7 @@ export default class Carrinho {
     this.items = [];
 
     return {
-      total,
+      total: total.getAmount(),
       items,
     };
   }
--- src/lib/carrinho.spec.js
+++ src/lib/carrinho.spec.js
@@ -17,8 +17,8 @@ describe("Carrinho de compras", () => {
   });
 
   describe("PegarTotal", () => {
-    it("Deve retornar 0 quando o getTotal() for executado a primeira vez", () => {
-      expect(carrinho.pegarTotal()).toEqual(0);
+    it("Deve retornar 0 quando o pegarTotal() for executado a primeira vez", () => {
+      expect(carrinho.pegarTotal().getAmount()).toEqual(0);
     });
 
     it("Deve multiplicar a quantidade de preço e receber a quantia total", () => {
@@ -27,7 +27,7 @@ describe("Carrinho de compras", () => {
         quantidade: 2,
       };
       carrinho.add(item);
-      expect(carrinho.pegarTotal()).toEqual(70776);
+      expect(carrinho.pegarTotal().getAmount()).toEqual(70776);
     });
 
     it("Deve garantir que o tenha apenas um produto no carrinho por vez", () => {
@@ -41,7 +41,7 @@ describe("Carrinho de compras", () => {
         quantidade: 1,
       });
 
-      expect(carrinho.pegarTotal()).toEqual(35388);
+      expect(carrinho.pegarTotal().getAmount()).toEqual(35388);
     });
 
     it("Deve atualizar o total quando remover um produto", () => {
@@ -57,7 +57,7 @@ describe("Carrinho de compras", () => {
 
       carrinho.remover(produto);
 
-      expect(carrinho.pegarTotal()).toEqual(41872);
+      expect(carrinho.pegarTotal().getAmount()).toEqual(41872);
     });
   });
 
@@ -107,7 +107,7 @@ describe("Carrinho de compras", () => {
         quantidade: 2,
       });
 
-      expect(carrinho.pegarTotal()).toBeGreaterThan(0);
+      expect(carrinho.pegarTotal().getAmount()).toBeGreaterThan(0);
       expect(carrinho.checkout()).toMatchInlineSnapshot(`
         Object {
           "items": Array [
@@ -131,6 +131,20 @@ describe("Carrinho de compras", () => {
       `);
     });
 
+    it("Deve formatar o valor do summary", () => {
+      carrinho.add({
+        produto,
+        quantidade: 5,
+      });
+
+      carrinho.add({
+        produto: produto2,
+        quantidade: 3,
+      });
+
+      expect(carrinho.summary().formatado).toEqual("R$3,025.56");
+    });
+
     it("Deve zerar o carrinho quando o checkout for chamado", () => {
       carrinho.add({
         produto: produto2,
@@ -139,7 +153,117 @@ describe("Carrinho de compras", () => {
 
       carrinho.checkout();
 
-      expect(carrinho.pegarTotal()).toEqual(0);
+      expect(carrinho.pegarTotal().getAmount()).toEqual(0);
+    });
+  });
+
+  describe("Desconto", () => {
+    it("Deve aplicar desconto quando a quantidade mínima é passada", () => {
+      const condicao = {
+        porcentagem: 30,
+        minimo: 2,
+      };
+
+      carrinho.add({
+        produto,
+        condicao,
+        quantidade: 3,
+      });
+
+      expect(carrinho.pegarTotal().getAmount()).toEqual(74315);
+    });
+
+    it("Deve não aplicar desconto quando a quantidade mínima é menor ou igual ", () => {
+      const condicao = {
+        porcentagem: 30,
+        minimo: 2,
+      };
+
+      carrinho.add({
+        produto,
+        condicao,
+        quantidade: 2,
+      });
+
+      expect(carrinho.pegarTotal().getAmount()).toEqual(70776);
+    });
+
+    it("Deve aplicar desconto baseado em quantidade", () => {
+      const condicao = {
+        quantidade: 2,
+      };
+
+      carrinho.add({
+        produto,
+        condicao,
+        quantidade: 4,
+      });
+
+      expect(carrinho.pegarTotal().getAmount()).toEqual(70776);
+    });
+
+    it("Deve não aplicar desconto baseado em quantidade quando condição é menor", () => {
+      const condicao = {
+        quantidade: 2,
+      };
+
+      carrinho.add({
+        produto,
+        condicao,
+        quantidade: 1,
+      });
+
+      expect(carrinho.pegarTotal().getAmount()).toEqual(35388);
+    });
+
+    it("Deve aplicar desconto para quantidades ímpares", () => {
+      const condicao = {
+        quantidade: 2,
+      };
+
+      carrinho.add({
+        produto,
+        condicao,
+        quantidade: 5,
+      });
+
+      expect(carrinho.pegarTotal().getAmount()).toEqual(106164);
+    });
+
+    it("Deve receber duas condições e aplicar a condição mais vantajosa ao cliente, PRIMEIRO CASO", () => {
+      const condicao1 = {
+        porcentagem: 30,
+        minimo: 2,
+      };
+      const condicao2 = {
+        quantidade: 2,
+      };
+
+      carrinho.add({
+        produto,
+        condicao: [condicao1, condicao2],
+        quantidade: 5,
+      });
+
+      expect(carrinho.pegarTotal().getAmount()).toEqual(106164);
+    });
+
+    it("Deve receber duas condições e aplicar a condição mais vantajosa ao cliente, SEGUNDO CASO", () => {
+      const condicao1 = {
+        porcentagem: 80,
+        minimo: 2,
+      };
+      const condicao2 = {
+        quantidade: 2,
+      };
+
+      carrinho.add({
+        produto,
+        condicao: [condicao1, condicao2],
+        quantidade: 5,
+      });
+
+      expect(carrinho.pegarTotal().getAmount()).toEqual(35388);
     });
   });
 });
